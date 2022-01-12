209a1e9a5af2e903e55a83505a2afc1c320e0fd4
Lucien Carbonare Jospin <lucien.jospin@ext.hdi.com.br>
2022-01-12 12:05:43 -0300

    Customize port

--- src/main/java/com/spring/reative/reactivewebservice/ReactiveWebServiceApplication.java
+++ src/main/java/com/spring/reative/reactivewebservice/ReactiveWebServiceApplication.java
@@ -1,6 +1,5 @@
 package com.spring.reative.reactivewebservice;
 
-import com.spring.reative.reactivewebservice.hello.client.GreetingClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -8,9 +7,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 @SpringBootApplication
 public class ReactiveWebServiceApplication {
 
-	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(ReactiveWebServiceApplication.class, args);
-		GreetingClient greetingClient = context.getBean(GreetingClient.class);
-		System.out.println(">> message = " + greetingClient.getMessage().block());
-	}
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(ReactiveWebServiceApplication.class, args);
+    }
 }
--- src/main/java/com/spring/reative/reactivewebservice/hello/client/GreetingClient.java
+++ src/main/java/com/spring/reative/reactivewebservice/hello/client/GreetingClient.java
@@ -1,18 +1,23 @@
 package com.spring.reative.reactivewebservice.hello.client;
 
 import com.spring.reative.reactivewebservice.hello.pojo.Greeting;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+
+import java.util.Objects;
 
 @Component
 public class GreetingClient {
 
     private final WebClient client;
 
-    public GreetingClient(WebClient.Builder builder) {
-        this.client = builder.baseUrl("http://localhost:8080").build();
+
+    public GreetingClient(WebClient.Builder builder, @Value("${server.port}") int serverPort) {
+        String baseUrl = String.format("http://localhost:%d", serverPort);
+        this.client = builder.baseUrl(baseUrl).build();
     }
 
     public Mono<String> getMessage() {
